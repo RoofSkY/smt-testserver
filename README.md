e70b35f21d82e8b65c1deecbe43ecf6af67e048d
RoofSkY <codenamedoobie@naver.com>
2024-12-11 21:11:40 +0900

    add some js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Test
+
+-for node.js v22.12.0
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function () {
+    console.log('Server running on port 8080');
+});
+
+app.get('/test', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/htmltest', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/md', function (req, res) {
+    res.sendFile(__dirname + '/md.html');
+});
